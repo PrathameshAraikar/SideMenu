705bbae9c6214bc173d2c0c9cc89d9750e3edd92
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2022-06-04 15:21:12 +0530

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Prathamesh Araikar on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
